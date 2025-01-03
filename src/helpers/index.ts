b90e80b707f0dbb7c6bba511ac53e65dd4f188c0
doug-stewart <hi@dou.gg>
2025-01-03 14:39:19 -0800

    Initial commit

---
+++ src/helpers/index.ts
@@ -0,0 +1,12 @@
+/*
+  Creates a more clean, consistent URI by:
+   - replacing spaces with hyphens so they don't get encoded
+   - encoding the string to make URL-safe
+   - removing encoded characters (they don't add meaning)
+   - removing additional hyphens (they are noise)
+  */
+
+export const encodeStringForId = (string: string) =>
+  encodeURIComponent(string.toLocaleLowerCase().replace(/ /g, '-'))
+    .replace(/%\w+/g, '')
+    .replace(/--/g, '-');
