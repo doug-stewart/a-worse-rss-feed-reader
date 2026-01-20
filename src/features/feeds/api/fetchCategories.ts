13c262caf18d9debf0838bc5ea41f465309d42db
Doug Stewart <hi@dou.gg>
2026-01-20 14:40:15 -0800

    Update feed links

--- src/features/feeds/api/fetchCategories.ts
+++ src/features/feeds/api/fetchCategories.ts
@@ -4,15 +4,15 @@ import DOMPurify from 'dompurify';
 import type { CategoriesObj, CategoryObj } from '../types';
 
 export const fetchCategories = async (): Promise<CategoriesObj> => {
-    const settings = await axios.get('/categories.json');
+    const categories = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/categories.json`);
 
-    const collatedCategories = settings.data.categories.map((category: CategoryObj) => ({
+    const collatedCategories = categories.data.categories.map((category: CategoryObj) => ({
         ...category,
         text: DOMPurify.sanitize(category.text),
     }));
 
     return {
-        categoryOrder: settings.data.categoryOrder || [],
+        categoryOrder: categories.data.categoryOrder || [],
         categories: collatedCategories || [],
     };
 };
