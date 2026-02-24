641112984b2a9a64411d37909d402bf4f79d17f0
Doug Stewart <hi@dou.gg>
2026-02-24 14:58:36 -0800

    Update search params to take an array of categories and feeds instead of singular feeds and categories

--- src/routes/feeds.tsx
+++ src/routes/feeds.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 type FeedSearchParams = {
-    category?: number;
-    feed?: number;
+    category?: Array<number>;
+    feed?: Array<number>;
     range?: string;
 };
 
