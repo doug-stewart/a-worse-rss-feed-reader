a4f917db280953a55b06b7a15f121afe096df81d
Doug Stewart <hi@dou.gg>
2026-07-31 15:36:48 -0700

    Add API URL, fix max articles definition

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const MAX_ARTICLES = parseInt(import.meta.env.VITE_MAX_ARTICLES || '20');
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+export const MAX_ARTICLES = Number(import.meta.env.VITE_MAX_ARTICLES || "20");
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
