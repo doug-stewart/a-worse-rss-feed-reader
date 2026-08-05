99e44b354c809bb4dc26782736b9de794890da0f
Doug Stewart <hi@dou.gg>
2026-08-04 21:10:26 -0700

    Add inline SVG type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,10 @@
 export type LayoutConsts = "row" | "card" | "full";
+
+export type InlineSVG = React.FunctionComponent<
+  React.SVGProps<SVGSVGElement> & {
+    title?: string;
+    titleId?: string;
+    desc?: string;
+    descId?: string;
+  }
+>;
