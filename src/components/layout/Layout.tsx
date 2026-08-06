23c80004d7ea7b443f81036106a7f010279bfe6f
Doug Stewart <hi@dou.gg>
2026-08-06 16:24:46 -0700

    Add scroll context to layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,13 @@
 import { Navigate, useLocation, useSearch } from "@tanstack/react-router";
-import { type PropsWithChildren, useLayoutEffect } from "react";
+import { type PropsWithChildren, useLayoutEffect, useRef } from "react";
 import Logo from "@/assets/logo.svg?react";
+import ScrollContext from "@/context/ScrollContext";
 import { useAuth } from "@/features/auth/hooks/useAuth";
 import { scrollMainToTop } from "@/features/feeds/helpers/scrollMainToTop";
 import styles from "./Layout.module.css";
 
 export const Layout = ({ children }: PropsWithChildren) => {
+  const mainRef = useRef(null);
   const { isLoaded, isAuthenticated } = useAuth();
   const pathname = useLocation({
     select: (location) => location.pathname,
@@ -25,13 +27,15 @@ export const Layout = ({ children }: PropsWithChildren) => {
   return shouldRedirect ? (
     <Navigate to="/" />
   ) : (
-    <>
+    <ScrollContext value={mainRef}>
       <header className={styles.header}>
         <h1>
           <Logo title="A Worse RSS App" />
         </h1>
       </header>
-      <main className={styles.main}>{children}</main>
-    </>
+      <main className={styles.main} ref={mainRef}>
+        {children}
+      </main>
+    </ScrollContext>
   );
 };
