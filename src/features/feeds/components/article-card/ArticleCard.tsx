fa3d180a288f092878cebb08f77e8dbf481e2159
Doug Stewart <hi@dou.gg>
2026-08-06 16:25:36 -0700

    Add basic controls for marking articles as read/unread and locking them in an unread state, improve scroll intersection observer to use scroll parent

--- src/features/feeds/components/article-card/ArticleCard.tsx
+++ src/features/feeds/components/article-card/ArticleCard.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx";
 import { useLayoutEffect, useRef, useState } from "react";
+import { useScrollContext } from "@/hooks/useScrollContext";
 import type { LayoutConsts } from "@/types";
+import { useArticles } from "../../hooks/useArticles";
 import { useFeeds } from "../../hooks/useFeeds";
 import type { Article } from "../../types";
 import styles from "./ArticleCard.module.css";
@@ -16,44 +18,77 @@ export const ArticleCard = ({ article, layout, onRead }: ArticleCardProps) => {
   const { url, title, published_at, cover, body, summary } = article || {};
 
   const { feeds } = useFeeds();
+  const { markRead, markUnread } = useArticles();
   const parent = feeds.find((feed) => feed.id === article.feed_id)?.name || null;
+
+  const { root: scrollRoot } = useScrollContext();
 
   const wrapper = useRef<HTMLElement | null>(null);
   const [height, setHeight] = useState(0);
   const [isRead, setIsRead] = useState(article.viewed || false);
   const [isVisible, setVisible] = useState(false);
+  const [keepUnread, setKeepUnread] = useState(false);
+
+  const markArticleRead = () => {
+    if (keepUnread) return;
+    setIsRead(true);
+    markRead.mutate([article.id]);
+  };
+
+  const markArticleUnread = () => {
+    setIsRead(false);
+    setKeepUnread(true);
+    markUnread.mutate([article.id]);
+  };
+
+  const lockUnread = () => {
+    if (isRead) {
+      markUnread.mutate([article.id]);
+      setIsRead(false);
+    }
+    setKeepUnread(true);
+  };
+
+  const unlockUnread = () => {
+    setKeepUnread(false);
+  };
 
   useLayoutEffect(() => {
-    if (!wrapper.current) return;
+    if (!wrapper.current || !scrollRoot) return;
 
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        const onScreen = entry.isIntersecting;
-        setVisible(onScreen);
+    const handleIntersection = (entryies: Array<IntersectionObserverEntry>) => {
+      const [entry] = entryies;
+      const onScreen = entry.isIntersecting;
+      setVisible(onScreen);
 
-        if (!onScreen) {
-          setHeight(wrapper.current?.clientHeight || 0);
-        }
+      if (!onScreen) {
+        setHeight(wrapper.current?.clientHeight || 0);
+      }
 
-        // IntersectionObserver fires when the card is partially off the screen
-        // and we only want to count when it's fully off the screen.
-        if (!article.viewed && !onScreen && entry.boundingClientRect.top <= 0) {
-          setIsRead(true);
-          onRead(article.id);
-        }
-      },
-      {
-        threshold: 0,
-        // Use excessive bottom scroll margin to give us time
-        // to conditionally load additional assets.
-        rootMargin: "0px 0px 3000px 0px",
-      },
-    );
+      // IntersectionObserver fires when the card is partially off the screen
+      // and we only want to count when it's fully off the screen.
+      if (
+        keepUnread === false &&
+        onScreen === false &&
+        article.viewed === false &&
+        entry.boundingClientRect.top <= 0
+      ) {
+        setIsRead(true);
+        onRead(article.id);
+      }
+    };
 
+    const options = {
+      root: scrollRoot,
+      threshold: 0,
+      rootMargin: "1000px 0px 1000px 0px",
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
     observer.observe(wrapper.current);
 
     return () => observer.disconnect();
-  }, [onRead, article.id, article.viewed]);
+  }, [onRead, keepUnread, scrollRoot, article.id, article.viewed]);
 
   return (
     <article
@@ -79,6 +114,26 @@ export const ArticleCard = ({ article, layout, onRead }: ArticleCardProps) => {
           url={url}
         />
       ) : null}
+      <menu className={styles.actions}>
+        {isRead ? (
+          <button disabled={keepUnread} onClick={markArticleUnread} type="button">
+            Unread
+          </button>
+        ) : (
+          <button disabled={keepUnread} onClick={markArticleRead} type="button">
+            Read
+          </button>
+        )}
+        {keepUnread ? (
+          <button onClick={unlockUnread} type="button">
+            Unlock
+          </button>
+        ) : (
+          <button onClick={lockUnread} type="button">
+            Lock
+          </button>
+        )}
+      </menu>
     </article>
   );
 };
